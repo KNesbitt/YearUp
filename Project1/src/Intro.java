b0eb0ed2b482b95d5030c225850b6e191158bc50
KNesbitt <knesbitt@atl.yearup.org>
2015-09-22 11:32:32 -0400

    My first Java program.

---
+++ Project1/src/Intro.java
@@ -0,0 +1,11 @@
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Kinsler Nesbitt");
+		System.out.println("My favorite Movie is The Matrix");
+		System.out.println("Fun fact about me is that I can play the drums");
+	}
+
+}
