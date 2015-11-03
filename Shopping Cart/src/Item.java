4e334b878b6367874cbe27941eea1d81279acb30
KNesbitt <knesbitt@atl.yearup.org>
2015-11-03 11:30:29 -0500

    Done

---
+++ Shopping Cart/src/Item.java
@@ -0,0 +1,20 @@
+
+public class Item 
+{
+
+		String m_name = null;
+		double m_price = 0.0;
+		int m_quantity = 0;
+		// 
+	
+		void Item (String name, double price, int quantity)
+		{	
+			String m_name = name;
+			double m_price = price;
+			int m_quantity = quantity;
+			//
+		}
+	
+	
+	
+}	
